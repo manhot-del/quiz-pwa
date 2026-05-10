2d9554e4286944c687bd5f467f91ee9ac32dd34b
manhot-del <manhotadsl@gmail.com>
2026-05-10 16:41:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "quiz-pwa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./questions.json",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
+  );
+});
